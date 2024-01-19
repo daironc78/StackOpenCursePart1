3b1185108be08d650d6b19d9557e63b509464fe4
Dairon <daironc78@gmail.com>
2024-01-18 19:35:32 -0500

    1.5

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,35 +1,87 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
+const Part = (props) => {
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div>
+      <p>
+       {props.name}, {props.exercises}
       </p>
-    </>
+    </div>
   )
 }
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+
+
+
+  let exer1 = 0
+  let exername
+  course.parts.map(value => exer1 += value.exercises)
+
+  course.parts.map(value => exername = value.name)
+  
+  const Header = () => {
+    return (
+    <div>
+    
+    {course.name}
+    
+    </div>
+    )
+  }
+  
+  const Total = () => {
+
+    
+
+    return (
+    <div>
+    
+           <p>Total exercises is {exer1} </p>
+    
+    </div>
+    )
+  }
+  
+  const Content = () => {
+    
+    return (
+    
+    <div>
+      {
+        course.parts.map(value => <Part name={ value.name } exercises={ value.exercises }/>)
+      }
+    </div>
+    
+    )
+  }
+
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={course} />
+      <Total parts={course} />
+    </div>
+  )
+  
+    }
 
 export default App
