1eb9aff88e0fa9f52a2b8cd1d78f54b52228f616
Dairon <daironc78@gmail.com>
2024-01-18 19:21:39 -0500

    part1_A

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,9 +1,48 @@
 const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  const Content = () => {
+
+    return(
+      <div>
+        <p>{part1}, {exercises1}</p>
+            <p>{part2}, {exercises2}</p>
+                <p>{part3}, {exercises3}</p>
+              </div>
+    )}
+
+    const Total = () => {
+
+      return(
+        <div>
+          <p>Number of exercises is {exercises1 + exercises2 + exercises3}</p>
+        </div>
+      )}
+
+      const Header = () => {
+
+        return(
+          <div>
+            <p>{course}</p>
+          </div>
+        )}
+
   return (
     <div>
-      <p>Hello world</p>
-    </div>
-  )
+    <Header course={course} />
+    <Content  />
+    <Total  />
+  </div>
+)
+
+
 }
+
 
 export default App
