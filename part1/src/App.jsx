fafc95c9e203c9d6bc9970357bdf3e40ba07d207
Dairon <daironc78@gmail.com>
2024-01-22 19:22:38 -0500

    Repaso Descomposicion, Explicacion de la contador con console

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 const App = () => {
 
-  const [ counter, setCounter ] = useState(0)
 
-  const Display = (props) => {
-    return (
-      <div>{props.counter}</div>
-    )
-  }
+
+  const [ counter, setCounter ] = useState(0)
+  console.log('rendering with counter value', counter)
+
+
+
+  const Display = ({ counter }) => <div>{counter}</div>
 
   const Button = (props) => {
     return (
@@ -17,9 +18,23 @@ const App = () => {
     )
   }
 
-  const increaseByOne = () => setCounter(counter + 1)
-  const decreaseByOne = () => setCounter(counter - 1)
-  const setToZero = () => setCounter(0)
+
+
+  const increaseByOne = () => {
+    console.log('increasing, value before', counter)
+    setCounter(counter + 1)}
+
+
+  const decreaseByOne = () => {
+    console.log('decreasing, value before', counter)
+    setCounter(counter - 1)}
+
+
+  const setToZero = () => {
+    console.log('resetting to zero, value before', counter)
+    setCounter(0)}
+
+
 
   return (
     <div>
