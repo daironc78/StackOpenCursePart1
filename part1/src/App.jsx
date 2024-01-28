85cfc0673552d540e22eb79f07121e6a8587d2e8
Dairon <daironc78@gmail.com>
2024-01-27 19:23:26 -0500

    Reglas de Hooks Manejo de eventos revisados funcion que devuelve una funcion

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,34 +1,21 @@
 import { useState } from 'react'
+
 const App = () => {
-  
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
-  const [total, setTotal] = useState(0)
+  const [value, setValue] = useState(10)
 
-    const handleLeftClick = () => {
-      setAll(allClicks.concat('L'))
-      const updatedLeft = left + 1
-      setLeft(updatedLeft)
-      setTotal(updatedLeft + right)
-  }
 
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    const updatedRight = right + 1
-    setRight(updatedRight)
-    setTotal(left + updatedRight)
-    
+  const setToValue = (newValue) => () => {
+    console.log('value now', newValue)  // imprime el nuevo valor en la consola
+    setValue(newValue)
   }
 
   return (
     <div>
-      {left}
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightClick}>right</button>
-      {right}
-      <p>{allClicks.join(' ')}</p>
-      <p>total {total}</p>
+      {value}
+
+      <button onClick={setToValue(1000)}>thousand</button>
+      <button onClick={setToValue(0)}>reset</button>
+      <button onClick={setToValue(value + 1)}>increment</button>
     </div>
   )
 }
