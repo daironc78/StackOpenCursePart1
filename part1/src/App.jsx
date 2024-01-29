007dbd62b4a5d2a787d91e30ffad23ec1ba513f0
Dairon <daironc78@gmail.com>
2024-01-28 19:44:08 -0500

    Repaso Ejercicios 1.7 y parte del 1.8

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,21 +1,51 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [value, setValue] = useState(10)
+  // guarda los clics de cada botón en su propio estado
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [all, setAll] = useState([])
 
-
-  const setToValue = (newValue) => () => {
-    console.log('value now', newValue)  // imprime el nuevo valor en la consola
-    setValue(newValue)
+  const setToGood = () => {
+    console.log('good', good)
+    setAll(good + neutral + bad)
+    setGood(good + 1)
   }
+
+  const setToNeutral = () => {
+    console.log('neutral', neutral)
+    setAll(good + neutral + bad)
+    setNeutral(neutral + 1)
+  }
+
+  const setToBad = () => {
+    console.log('bad', bad)
+    setAll(good + neutral + bad)
+    setBad(bad + 1)
+  }
+
+  const Button = (props) => (
+    <button onClick={props.handleClick}>
+    {props.text}
+    </button>
+  )
 
   return (
     <div>
-      {value}
 
-      <button onClick={setToValue(1000)}>thousand</button>
-      <button onClick={setToValue(0)}>reset</button>
-      <button onClick={setToValue(value + 1)}>increment</button>
+      <h1>give feedback</h1>
+      <Button handleClick={() => setToGood(good + 1)} text="Good" />
+      <Button handleClick={() => setToNeutral(neutral + 1)} text="Neutral" />
+      <Button handleClick={() => setToBad(bad + 1)} text="Bad" />
+      <h1>stadistics</h1>
+      <p>good {good}</p>
+      <br />
+      <p>neutral {neutral}</p>
+      <br />
+      <p>bad {bad}</p>
+      {}
+
     </div>
   )
 }
